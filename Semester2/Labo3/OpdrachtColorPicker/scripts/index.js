3184dd8edc5fd54dd1d0ae01fa0cbe04c3c70e05
ManoBouttelisier <mano.bouttelisier@student.vives.be>
2024-03-03 09:42:49 +0100

    Labo 3
    1th push

---
+++ Semester2/Labo3/OpdrachtColorPicker/scripts/index.js
@@ -0,0 +1,33 @@
+let setup;
+setup = () => {
+    const sliders = document.getElementsByClassName("slider");
+    for(let i = 0; i < sliders.length;i++){
+        sliders[i].addEventListener("change", changeColor);
+        sliders[i].addEventListener("input", changeColor);
+    }
+    changeColor();
+};
+
+let changeColor;
+changeColor = () => {
+    const colorSquare = document.getElementById("CP-square")
+    const redSlider = document.getElementById("red");
+    const greenSlider = document.getElementById("green");
+    const blueSlider = document.getElementById("blue");
+
+    let red = redSlider.value;
+    let green = greenSlider.value;
+    let blue = blueSlider.value;
+    let bgColor = `rgb(${red} , ${green}, ${blue})`;
+
+    let valueRed = document.getElementById("valueRed");
+    let valueGreen = document.getElementById("valueGreen");
+    let valueBlue = document.getElementById("valueBlue");
+
+    valueRed.textContent = " Red: " + red;
+    valueGreen.textContent = " Green: " + green;
+    valueBlue.textContent = " Blue: " + blue;
+
+    colorSquare.style.backgroundColor = bgColor;
+}
+window.addEventListener("load", setup);
